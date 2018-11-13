54c935fd2334206bf04afaf120ab68c3d7327339
Cara Hill <cara@rabidtech.co.nz>
2018-11-13 21:05:16 +1300

    Refactored

--- pangram.js
+++ pangram.js
@@ -1,27 +1,15 @@
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
 function isPangram(arg) {
-  let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
   if(arg == '') { return false; }
-
-  let result = [];
 
   let argArray = arg.toLowerCase().replace(/[^a-z]+/g, '').split('');
 
-  argArray.forEach(function(letter) {
-    result.push(alphabet.includes(letter));
+  let resultArray = alphabet.map(function(letter) {
+     return argArray.includes(letter);
   });
 
-  let allIncluded = [];
-
-  alphabet.forEach(function(letter) {
-    allIncluded.push(argArray.includes(letter));
-  });
-
-  if(result.includes(false) || allIncluded.includes(false)) {
-    return false;
-  } else {
-    return true;
-  }
+  return !resultArray.includes(false);
 }
 
 export { isPangram }
